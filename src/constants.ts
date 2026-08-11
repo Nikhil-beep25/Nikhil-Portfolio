144678e2b064cdf9ce06fe5d0766b714a60e78dd
Nikhil-beep25 <nikhilbhadauriya2500@gmail.com>
2026-08-11 17:31:15 +0530

    Update Instagram URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const INSTAGRAM_URL = "https://www.instagram.com/itsnikhil_tech?utm_source=qr&igsh=ZnZ2Y2Jybmx4OGw2";
+export const INSTAGRAM_URL = "https://www.instagram.com/itsnikhil_tech";
